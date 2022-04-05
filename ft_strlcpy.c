eb1b05859044cca8467bd303b85283990594f417
Daniel ricardo Rodriguez tautiva <danielro@c3r1s3.42urduliz.com>
2022-04-05 11:53:42 +0200

    cambios

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,26 +6,29 @@
 /*   By: danielro <danielro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/01 15:57:29 by danielro          #+#    #+#             */
-/*   Updated: 2022/04/04 12:51:05 by danielro         ###   ########.fr       */
+/*   Updated: 2022/04/05 11:47:39 by danielro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <ctype.h>
+#include <string.h>
+
+int	ft_strlen(const char *s);
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-	char	*i;
-	int		a;
+	int				a;
+	unsigned int	b;
 
-	i = (char *)src;
-	a = 0;
-	while (src[a])
-		a++;
-	if (dstsize-- > 0)
+	a = ft_strlen(src);
+	b = 0;
+	if (dstsize != 0)
 	{
-		while (dstsize-- && *dst)
-			*dst++ = *i++;
-		*dst++ = '\0';
+		while (src[b] && b < (dstsize - 1))
+		{
+			dst[b] = src[b];
+			b++;
+		}
+		dst[b] = '\0';
 	}
 	return (a);
 }
