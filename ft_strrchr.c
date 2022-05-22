b4ac2c9b6d8cbe978e6ab6e419f73d3335cc801c
Daniel ricardo Rodriguez tautiva <danielro@c3r1s5.42urduliz.com>
2022-05-22 14:38:19 +0200

    entrega

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: danielro <danielro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/09 18:15:45 by danielro          #+#    #+#             */
-/*   Updated: 2022/05/21 17:56:31 by danielro         ###   ########.fr       */
+/*   Updated: 2022/05/22 14:26:17 by danielro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,14 @@
 char	*ft_strrchr(const char *s, int c)
 {
 	int			a;
-	const char	*b;
 
 	a = ft_strlen(s);
-	b = s;
 	s = s + a;
-	while (*s != *b && (char)c != *s)
+	while (a > 0 && (char)c != *s)
+	{
 		s--;
+		a--;
+	}
 	if (*s == (char)c)
 		return ((char *)s);
 	return (0);
